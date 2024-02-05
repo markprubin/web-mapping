7ad5ee67229c5476c8e21a159546ce52859d442a
Mark Rubin <mark.p.rubin@gmail.com>
2024-02-05 12:34:39 -0700

    adds feature group

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import folium
 
 map = folium.Map(location=[33.657029634028994, -112.08982501349456], zoom_start=16, tiles="CartoDB Voyager")
 
-map.add_child(folium.Marker(
+
+fg = folium.FeatureGroup(name='My Map')
+
+fg.add_child(folium.Marker(
     location=[33.657029634028994, -112.08982501349456],
     popup="Yo",
     icon=folium.Icon(color='red')))
+
+map.add_child(fg)
 
 
 map.save("Map1.html")
