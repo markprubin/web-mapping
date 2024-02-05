a9c8e6e62bfd7dd8f0db80fa41fa5940233c0d89
Mark Rubin <mark.p.rubin@gmail.com>
2024-02-05 12:33:06 -0700

    adds market

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import folium
 
-map = folium.Map(location=[80, -100], tiles="CartoDB Voyager")
+map = folium.Map(location=[33.657029634028994, -112.08982501349456], zoom_start=16, tiles="CartoDB Voyager")
+
+map.add_child(folium.Marker(
+    location=[33.657029634028994, -112.08982501349456],
+    popup="Yo",
+    icon=folium.Icon(color='red')))
+
+
 map.save("Map1.html")
