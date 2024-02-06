5958f16ef1aa935ab032fb914e37f4e010c9c5b7
Mark Rubin <mark.p.rubin@gmail.com>
2024-02-05 17:45:28 -0700

    updates popup with elevation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 data = pd.read_csv("Volcanoes.txt")
 lat = list(data['LAT'])
 lon = list(data['LON'])
-
+elev = list(data['ELEV'])
 
 map = folium.Map(location=[40.758701, -111.876183], zoom_start=6, tiles="CartoDB Voyager")
 
@@ -12,10 +12,10 @@ map = folium.Map(location=[40.758701, -111.876183], zoom_start=6, tiles="CartoDB
 fg = folium.FeatureGroup(name='My Map')
 
 
-for lat, lon in zip(lat, lon):
+for lat, lon, elev in zip(lat, lon, elev):
     fg.add_child(folium.Marker(
         location=[lat, lon],
-        popup="Volcano",
+        popup=f"{elev} meters",
         icon=folium.Icon(color='red')))
 
 map.add_child(fg)
