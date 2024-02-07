9f735161434ca72083a3e305300aab68c67ab405
Mark Rubin <mark.p.rubin@gmail.com>
2024-02-06 18:36:14 -0700

    adds layer control

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ Name: <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a
 Height: %s m
 """
 
-fg = folium.FeatureGroup(name='My Map')
+fgvol = folium.FeatureGroup(name='Volcanoes')
 
 
 for lat, lon, elev, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=info % (name, name, elev), width=200, height=100)
 
-    fg.add_child(folium.CircleMarker(
+    fgvol.add_child(folium.CircleMarker(
         location=[lat, lon],
         radius=6,
         popup=folium.Popup(iframe),
@@ -38,11 +38,15 @@ for lat, lon, elev, name in zip(lat, lon, elev, name):
         fill_color=color_elevation(elev), color = 'grey', fill=True, fill_opacity=0.7
         ))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+
+fgpop = folium.FeatureGroup(name='Population')
+fgpop.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
                             style_function=lambda x: {"fillColor": 'green' if x['properties']['POP2005'] < 10000000
                             else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
 
+map.add_child(fgvol)
+map.add_child(fgpop)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
